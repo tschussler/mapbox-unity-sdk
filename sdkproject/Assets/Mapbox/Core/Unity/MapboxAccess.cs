3ffc117f9cffb3203feb59f4fdd22200f9fd9810
Siyu Song <siyu.song@gmail.com>
2017-04-20 16:41:26 -0700

    MapboxAccess.cs - version A     - caches requests that should be sent but no token is available yet, which would lead to a failed request.

--- sdkproject/Assets/Mapbox/Core/Unity/MapboxAccess.cs
+++ sdkproject/Assets/Mapbox/Core/Unity/MapboxAccess.cs
@@ -1,132 +1,194 @@
+using System.Collections.Generic;
 namespace Mapbox.Unity
 {
-    using System.IO;
-    using UnityEngine;
-    using System;
-    using System.Collections;
-    using Mapbox.Geocoding;
-    using Mapbox.Directions;
-    using Mapbox.Platform;
-    using Mapbox.Unity.Utilities;
+	using System.IO;
+	using UnityEngine;
+	using System;
+	using System.Collections;
+	using Mapbox.Geocoding;
+	using Mapbox.Directions;
+	using Mapbox.Platform;
+	using Mapbox.Unity.Utilities;
 
-    public class MapboxAccess : IFileSource
-    {
-        readonly string _accessPath = Path.Combine(Application.streamingAssetsPath, Constants.Path.TOKEN_FILE);
+	/// <summary>
+	/// Object for retrieving an API token and making http requests.
+	/// Contains a lazy <see cref="T:Mapbox.Geocoding.Geocoder">Geocoder</see> and a lazy <see cref="T:Mapbox.Directions.Directions">Directions</see> for convenience.
+	/// </summary>
+	public class MapboxAccess : IFileSource
+	{
+		readonly string _accessPath = Path.Combine(Application.streamingAssetsPath, Constants.Path.TOKEN_FILE);
 
-        static MapboxAccess _instance = new MapboxAccess();
-        public static MapboxAccess Instance
-        {
-            get
-            {
-                return _instance;
-            }
-        }
+		static MapboxAccess _instance = new MapboxAccess();
 
-        MapboxAccess()
-        {
-            ValidateMapboxAccessFile();
-            LoadAccessToken();
-        }
+		Dictionary<string, Action<Response>> _cachedRequests = new Dictionary<string, Action<Response>>();
 
-        string _accessToken;
-        public string AccessToken
-        {
-            get
-            {
-                return _accessToken;
-            }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new InvalidTokenException("Please configure your access token in the menu!");
-                }
-                _accessToken = value;
-            }
-        }
+		/// <summary>
+		/// The singleton instance.
+		/// </summary>
+		public static MapboxAccess Instance
+		{
+			get
+			{
+				return _instance;
+			}
+		}
 
-        void ValidateMapboxAccessFile()
-        {
-            if (!Directory.Exists(Application.streamingAssetsPath) || !File.Exists(_accessPath))
-            {
-                throw new InvalidTokenException("Please configure your access token in the menu!");
-            }
-        }
+		MapboxAccess()
+		{
+			ValidateMapboxAccessFile();
+			LoadAccessToken();
+		}
 
-        void LoadAccessToken()
-        {
-#if  UNITY_EDITOR || !UNITY_ANDROID
-            AccessToken = File.ReadAllText(_accessPath);
-#else
-            Runnable.Run(LoadMapboxAccess());
-#endif
-        }
+		/// <summary>
+		/// The Mapbox API access token. 
+		/// See <see href="https://www.mapbox.com/mapbox-unity-sdk/docs/01-mapbox-api-token.html">Mapbox API Congfiguration in Unity</see>.
+		/// </summary>
+		string _accessToken;
+		public string AccessToken
+		{
+			get
+			{
+				return _accessToken;
+			}
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+				{
+					throw new InvalidTokenException("Please configure your access token in the menu!");
+				}
+				_accessToken = value;
+			}
+		}
 
-        IEnumerator LoadMapboxAccess()
-        {
-            var request = new WWW(_accessPath);
-            while (!request.isDone)
-            {
-                yield return 0;
-            }
-            AccessToken = request.text;
-        }
+		void ValidateMapboxAccessFile()
+		{
+			#if !UNITY_ANDROID
+			if (!Directory.Exists(Application.streamingAssetsPath) || !File.Exists(_accessPath))
+			{
+			throw new InvalidTokenException("Please configure your access token in the menu!");
+			}
+			#endif
+		}
 
-        public IAsyncRequest Request(string url, Action<Response> callback)
-        {
-            var uriBuilder = new UriBuilder(url);
+		/// <summary>
+		/// Loads the access token from <see href="https://docs.unity3d.com/Manual/StreamingAssets.html">StreamingAssets</see>.
+		/// </summary>
+		void LoadAccessToken()
+		{
+			#if  UNITY_EDITOR || !UNITY_ANDROID
+			AccessToken = File.ReadAllText(_accessPath);
+			#else
+			Runnable.Run(LoadMapboxAccess());
+			#endif
+		}
 
-            string accessTokenQuery = "access_token=" + AccessToken;
+		/// <summary>
+		/// Android-specific token file loading.
+		/// </summary>
+		IEnumerator LoadMapboxAccess()
+		{
+			var request = new WWW(_accessPath);
+			while (!request.isDone)
+			{
+				yield return null;
+			}
+			AccessToken = request.text;
+			SendCachedRequests();
+		}
 
-            if (uriBuilder.Query != null && uriBuilder.Query.Length > 1)
-            {
-                uriBuilder.Query = uriBuilder.Query.Substring(1) + "&" + accessTokenQuery;
-            }
-            else
-            {
-                uriBuilder.Query = accessTokenQuery;
-            }
+		void SendCachedRequests()
+		{
+			foreach (var request in _cachedRequests)
+			{
+				Request(request.Key, request.Value);
+				Debug.Log("MapboxAccess: " + "Request sent: " + request.Key);
+			}
 
-            return new HTTPRequest(uriBuilder.ToString(), callback);
-        }
+			_cachedRequests.Clear();
+		}
 
-        class InvalidTokenException : Exception
-        {
-            public InvalidTokenException(string message) : base(message)
-            {
-            }
-        }
+		/// <summary>
+		/// Makes an asynchronous url query.
+		/// </summary>
+		/// <returns>The request.</returns>
+		/// <param name="url">URL.</param>
+		/// <param name="callback">Callback.</param>
+		public IAsyncRequest Request(string url, Action<Response> callback)
+		{
+			if (string.IsNullOrEmpty(_accessToken))
+			{
+				if (!_cachedRequests.ContainsKey(url))
+				{
+					_cachedRequests.Add(url, callback);
+					Debug.Log("MapboxAccess: " + "No token, waiting . . . ");
+					return new CachedAsyncRequest();
+				}
+			}
 
-        /// <summary>
-        /// Lazy geocoder.
-        /// </summary>
-        Geocoder _geocoder;
-        public Geocoder Geocoder
-        {
-            get
-            {
-                if (_geocoder == null)
-                {
-                    _geocoder = new Geocoder(this);
-                }
-                return _geocoder;
-            }
-        }
+			Debug.Log("MapboxAccess: " + "Immediate request: " + url);
+			var uriBuilder = new UriBuilder(url);
 
-        /// <summary>
-        /// Lazy Directions.
-        /// </summary>
-        Directions _directions;
-        public Directions Directions
-        {
-            get
-            {
-                if (_directions == null)
-                {
-                    _directions = new Directions(this);
-                }
-                return _directions;
-            }
-        }
-    }
+			string accessTokenQuery = "access_token=" + AccessToken;
+
+			if (uriBuilder.Query != null && uriBuilder.Query.Length > 1)
+			{
+				uriBuilder.Query = uriBuilder.Query.Substring(1) + "&" + accessTokenQuery;
+			}
+			else
+			{
+				uriBuilder.Query = accessTokenQuery;
+			}
+
+			return new HTTPRequest(uriBuilder.ToString(), callback);
+		}
+
+		class InvalidTokenException : Exception
+		{
+			public InvalidTokenException(string message) : base(message)
+			{
+			}
+		}
+
+		class CachedAsyncRequest : IAsyncRequest
+		{
+			public void Cancel()
+			{
+				//TODO: bubble this to the real request.
+			}
+		}
+
+		Geocoder _geocoder;
+
+		/// <summary>
+		/// Lazy geocoder.
+		/// </summary>
+		public Geocoder Geocoder
+		{
+			get
+			{
+				if (_geocoder == null)
+				{
+					_geocoder = new Geocoder(this);
+				}
+				return _geocoder;
+			}
+		}
+
+		Directions _directions;
+
+		/// <summary>
+		/// Lazy Directions.
+		/// </summary>
+		public Directions Directions
+		{
+			get
+			{
+				if (_directions == null)
+				{
+					_directions = new Directions(this);
+				}
+				return _directions;
+			}
+		}
+	}
 }
